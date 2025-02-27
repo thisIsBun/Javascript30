42842419520a211f396d36ae5d913f12021d7c32
thisIsBun <bun.coding@gmail.com>
2025-02-27 16:46:01 +0800

    chore: setup Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
+import "./styles/tailwind.css";
+
 function App() {
-  return (
-    <>
-    </>
-  );
+  return <h1 className="p-4 pt-2">Hello world!</h1>;
 }
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   server: {
     open: true,
     port: 3000,
   },
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],
+});
