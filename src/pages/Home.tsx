a57a1f52eec06892fb69c60207ed64b3d8c405ab
thisIsBun <bun.coding@gmail.com>
2025-03-28 15:25:20 +0800

    feat: add challenge levels for each challenge and enhance challenge card styles.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,13 @@ import challenges from "../data/challenges";
 import { NavLink } from "react-router";
 import Logo from "../components/Logo";
 
+const challengingBackground = {
+  challenging: "#ed4f55",
+  medium: "#ed7d4f",
+  simple: "#268969",
+  TBC: "#000000",
+};
+
 const Home = () => {
   return (
     <>
@@ -37,21 +44,32 @@ const Home = () => {
 
 const ChallengeCard = () => {
   return (
-    <ul className="mx-auto grid max-w-6xl grid-cols-1 gap-8 px-4 py-12 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+    <ul className="mx-auto grid max-w-[1400px] grid-cols-1 gap-4 px-4 py-12 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
       {challenges.map((challenge) => (
-        <li key={challenge.day} className="flex flex-col items-center">
-          <NavLink to={`/challenges/${challenge.day}`}>
-            <div className="group relative w-full max-w-[280px] overflow-hidden">
-              <span className="absolute z-10 bg-black px-2 py-1 text-xs font-bold text-white shadow-md transition-opacity duration-300 group-hover:opacity-100">
+        <li
+          key={challenge.day}
+          className="flex flex-col items-center rounded-md border border-transparent hover:border-gray-600"
+        >
+          <NavLink to={`/challenges/${challenge.day}`} className="w-full">
+            <div className="group relative w-full max-w-full overflow-hidden">
+              <span className="absolute bottom-0 z-10 bg-black px-2 py-1 text-xs font-bold text-white shadow-md transition-opacity duration-300 group-hover:opacity-100">
                 Day {challenge.day}
+              </span>
+              <span
+                className={`absolute right-0 bottom-0 z-10 px-2 py-1 text-xs font-bold text-white shadow-md transition-opacity duration-300 group-hover:opacity-100`}
+                style={{
+                  background: challengingBackground[challenge.challengeLevel],
+                }}
+              >
+                {challenge.challengeLevel}
               </span>
               <img
                 src={challenge.img}
                 alt={challenge.title}
-                className="h-48 w-full object-cover transition-transform duration-300 group-hover:scale-110"
+                className="h-48 w-full rounded-t-md object-cover transition-transform duration-300 group-hover:scale-110"
               />
             </div>
-            <div className="mt-2 text-center">
+            <div className="my-3 text-center">
               <div className="flex items-center justify-center">
                 <span className="mx-2 text-sm font-semibold text-yellow-500">
                   {challenge.title}
